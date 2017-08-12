06ae350a69d1761af99bcbcf2b2fbfd23b796177
Jed Fox <git@twopointzero.us>
2017-08-12 12:30:57 -0400

    variable-name → custom ban-keywords

---
+++ eslint-rules/ban-keywords.js
@@ -0,0 +1,26 @@
+// logic based on http://astexplorer.net/#/Lmzgbm2iRq
+
+const BANNED_NAMES = 'any number string boolean never'.split(' ')
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'Do not use TypeScript types as variable names',
+      category: 'Best Practices',
+    },
+  },
+  create(context) {
+    return {
+      Identifier(node) {
+        if (
+          BANNED_NAMES.includes(node.name) &&
+          (node.parent.type !== 'MemberExpression' ||
+            node.parent.computed ||
+            node.parent.object === node)
+        ) {
+          context.report(node, `Do not use the variable name '${node.name}'`)
+        }
+      },
+    }
+  },
+}
